77b669547a3e75ddb443c4af4ebd9a74c7f30018
Zan Markan <zan.markan@gmail.com>
2015-10-18 11:56:25 +0100

    Initial commit

---
+++ app/src/main/java/com/zmarkan/birthday_apod/ApodActivityMain.java
@@ -0,0 +1,13 @@
+package com.zmarkan.birthday_apod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ApodActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apod_activity_main);
+    }
+}
